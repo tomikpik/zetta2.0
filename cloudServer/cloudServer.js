ccdec3a699bbd63ca93fce316ee92cdd1c808a67
pikous <tomas.pikous@gmail.com>
2015-12-03 22:07:30 +0100

    created server and cloud folders

--- cloudServer/cloudServer.js
+++ cloudServer/cloudServer.js
@@ -1,14 +1,9 @@
-var zetta = require('./zetta2.0-runtime/zetta');
+var zetta = require('../zetta2.0-runtime/zetta');
 
 var PORT = 1337;
 
-var templateScout = require('./esense/scouts/scout_template');
-
-
 zetta()
-  .name('eclub-iot-hub')
-  .link('http://zettor.sin.cvut.cz:1337')
-  .use(templateScout)
+  .name('eclub-iot-cloud')
   .listen(PORT, function(err) {
     if(err) {
       console.error(err);
