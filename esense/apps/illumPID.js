d79d40cbfe6b8b65b3a7f0d8268d5c884a787e22
Tom <tomas.pikous@gmail.com>
2016-06-01 11:23:18 +0000

    message

--- esense/apps/illumPID.js
+++ esense/apps/illumPID.js
@@ -12,14 +12,11 @@ module.exports = function(server) {
 	//filesystem libray   
     var fs = require('fs');
 
-<<<<<<< HEAD
-	var lightQuery = server.where({ id: '4DCC31640E0' });
-   	var illuminationQuery = server.where({ type: '156' });
-=======
+
     //queries for devices
 	var lightQuery = server.where({ type: '165' });
     var illuminationQuery = server.where({ type: '156' });
->>>>>>> e5bca83bb0af98437a0514b8b5dfac1a623f2372
+
 	var helperQuery = server.where({ type: '169' });
 	
     //controller
@@ -29,19 +26,10 @@ module.exports = function(server) {
 
     //check for changes on devices from queries
 	server.observe([lightQuery,illuminationQuery,helperQuery], function(light,illumination,helper){
-<<<<<<< HEAD
-     	
 
-		
-  	    
-     	light.streams.power.on('data', function(m) {
-     			//console.log("power",m.data);
-		});
-
-=======
         
         //update PID gains
->>>>>>> e5bca83bb0af98437a0514b8b5dfac1a623f2372
+
 		helper.streams.p.on('data', function(m) {
 			if(ctr!=undefined){
 				ctr.setTunings(helper.p,helper.i,helper.d);
@@ -56,47 +44,22 @@ module.exports = function(server) {
             
         //new illumination level from sensor received
 		illumination.streams.illumination.on('data', function(m) {
-<<<<<<< HEAD
-			if(helper.state!='intensity')return;
-=======
+
             //controller initialization
->>>>>>> e5bca83bb0af98437a0514b8b5dfac1a623f2372
+
 			if(ctr==undefined){
                 //load values from PID helper
 				ctr = new PID(m.data,helper.target,helper.p,helper.i,helper.d, 'direct');
-<<<<<<< HEAD
-								
 
-=======
 				//ctr = new PID(m.data,target,0.03,0.15,0.001, 'direct');							
->>>>>>> e5bca83bb0af98437a0514b8b5dfac1a623f2372
+
 				ctr.setMode(1);
 				ctr.setOutputLimits(-1023,1023);
 				ctr.initialize();
 			}
             
 			console.log("illum",m.data,"error:",Math.abs(m.data-helper.target));
-<<<<<<< HEAD
-			
 
-			ctr.setInput(m.data);
-    			ctr.compute();
-			input = input + Math.round(ctr.getOutput());
-			if(input<0)input=0;
-			if(input>1023)input=1023	
-
-
-			console.log("SET POWER TO: ",input);	
-			light.call('set-power',input);
-			
-			var text = new Date().getTime()+";"+m.data+";"+input+";\n";
-			fs.appendFile('log.csv',text,function (err) {
-				console.log(err);
-			});
-				
-
-			     			
-=======
 			//if lower than hysteresis
             if(Math.abs(m.data-helper.target)<0){
 				console.log("ok");
@@ -136,7 +99,7 @@ module.exports = function(server) {
 				
 			}
      			
->>>>>>> e5bca83bb0af98437a0514b8b5dfac1a623f2372
+
 		});
 	});
 
